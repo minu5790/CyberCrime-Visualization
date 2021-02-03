9259d2068f217188716a0c70e886b1ef5395e953
qfaq159@naver.com <qfaq159@naver.com>
2021-02-03 22:20:21 +0900

    feat: yealytotal plot

---
+++ src/yearlytotal.js
@@ -0,0 +1,69 @@
+const yearlytotal = () => {
+  const loadYealryCsv = () => {
+    Plotly.d3.csv(
+      "../cyber/csv/경찰청_월별 사이버범죄 발생건수와 검거건수_20190520.csv",
+      (data) => {
+        processData(data);
+      }
+    );
+  };
+  const processData = (allRows) => {
+    const x1 = [],
+      x2 = [],
+      y1 = [],
+      y2 = [];
+    const keys = Object.keys(allRows[0]).filter(
+      (e) => e !== "연도" && e !== "구분"
+    );
+
+    allRows.forEach((row) => {
+      const total = keys.reduce(
+        (acc, currValue) => acc + parseInt(row[currValue]),
+        0
+      );
+
+      if (row["구분"] === "발생건수") {
+        x1.push(row["연도"]);
+        y1.push(total);
+      } else if (row["구분"] === "검거건수") {
+        x2.push(row["연도"]);
+        y2.push(total);
+      }
+    });
+    makePlotly(x1, y1, x2, y2);
+  };
+
+  const makePlotly = (x1, y1, x2, y2) => {
+    const trace1 = {
+      x: x1,
+      y: y1,
+      type: "scatter",
+      name: "발생건수",
+    };
+
+    const trace2 = {
+      x: x2,
+      y: y2,
+      type: "scatter",
+      name: "검거건수",
+    };
+
+    const data = [trace1, trace2];
+    const config = { responsive: true };
+
+    Plotly.newPlot(
+      "yearly__total",
+      data,
+      {
+        title: "<b>연도별 총 합계</b>",
+        titlefont: {
+          size: 25,
+        },
+      },
+      config
+    );
+  };
+  loadYealryCsv();
+};
+
+yearlytotal();
